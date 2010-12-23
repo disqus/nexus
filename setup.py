1d92fee8c7e848648e423d343e4731a986c786de
David Cramer <dcramer@gmail.com>
2010-12-22 21:17:23 -0800

    Bump version to 0.1.1 for pypi

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class mytest(test):
 
 setup(
     name='nexus',
-    version='0.1.0',
+    version='0.1.1',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/nexus',
