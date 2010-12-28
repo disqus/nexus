2518505372fdc444ae23dc24658744a32d876e77
David Cramer <dcramer@gmail.com>
2010-12-27 21:35:09 -0800

    Template module wasnt needed as Flask handles this

--- nexus/utils/template.py
+++
@@ -1,7 +0,0 @@
-from jinja2 import Environment, PackageLoader
-
-env = Environment(loader=PackageLoader('nexus', 'templates'))
-
-def render_template(template, context):
-    template = env.get_template(template)
-    return template.render(context)
