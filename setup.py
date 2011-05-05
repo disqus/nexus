dd7b1d78583c068b165efc3361463b340165d9ba
David Cramer <dcramer@gmail.com>
2011-05-05 16:32:12 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class mytest(test):
 
 setup(
     name='nexus',
-    version='0.1.3',
+    version='0.1.4',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/nexus',
