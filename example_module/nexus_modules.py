7ea6903de1c393396b6ca34dff95574bb7225fd3
David Cramer <dcramer@gmail.com>
2011-12-19 15:58:38 -0800

    More CSRF fixes (UGH)

--- example_module/nexus_modules.py
+++ example_module/nexus_modules.py
@@ -3,24 +3,24 @@ import nexus
 class HelloWorldModule(nexus.NexusModule):
     home_url = 'index'
     name = 'hello-world'
-    
+
     def get_title(self):
         return 'Hello World'
-    
+
     def get_urls(self):
         from django.conf.urls.defaults import patterns, url
 
         urlpatterns = patterns('',
             url(r'^$', self.as_view(self.index), name='index'),
         )
-        
+
         return urlpatterns
-    
+
     def render_on_dashboard(self, request):
         return self.render_to_string('nexus/example/dashboard.html', {
             'title': 'Hello World',
         })
-    
+
     def index(self, request):
         return self.render_to_response("nexus/example/index.html", {
             'title': 'Hello World',
