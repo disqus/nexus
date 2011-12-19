62d6aa6844e15e87bd37ab55e0d999495faf2729
David Cramer <dcramer@gmail.com>
2011-12-19 14:43:17 -0800

    Add version to footer

--- nexus/templatetags/nexus_helpers.py
+++ nexus/templatetags/nexus_helpers.py
@@ -1,35 +1,42 @@
 from django import template
-from django.core.urlresolvers import reverse
 from django.utils.datastructures import SortedDict
 
+import nexus
 from nexus import conf
 from nexus.modules import NexusModule
 
 register = template.Library()
 
+
 def nexus_media_prefix():
     return conf.MEDIA_PREFIX.rstrip('/')
 register.simple_tag(nexus_media_prefix)
 
+
+def nexus_version():
+    return nexus.VERSION
+register.simple_tag(nexus_version)
+
+
 def show_navigation(context):
     site = context.get('nexus_site', NexusModule.get_global('site'))
     request = NexusModule.get_request()
-    
+
     category_link_set = SortedDict([(k, {
         'label': v,
         'links': [],
     }) for k, v in site.get_categories()])
-    
+
     for namespace, module in site._registry.iteritems():
         module, category = module
-        
+
         if module.permission and not request.user.has_perm(module.permission):
             continue
 
         home_url = None
         if 'request' in context:
             home_url = module.get_home_url(context['request'])
-        
+
         if not home_url:
             continue
 
